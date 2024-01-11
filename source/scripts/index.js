1efe74622021e6de5b1305040416a82a37f90a4e
Anastasia <an.gorskova.bg@gmail.com>
2024-01-11 23:43:49 +0100

    исправлят ссылку на кнопку в менюшке

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,21 +1,21 @@
 /* в этот файл добавляет скрипты*/
-// document.addEventListener("DOMContentLoaded", function() {
-//   let menuButtons = document.querySelectorAll('.header__menu-button');
-//   let navigation = document.querySelector('.main-navigation__list');
+document.addEventListener("DOMContentLoaded", function() {
+  let menuButtons = document.querySelectorAll('.header__menu-button');
+  let navigation = document.querySelector('.main-navigation__list');
 
-//   menuButtons.forEach(function(menuButton) {
-//     menuButton.addEventListener('click', function() {
-//       menuButton.classList.toggle('header__menu-button--closed');
-//       menuButton.classList.toggle('header__menu-button--opened');
+  menuButtons.forEach(function(menuButton) {
+    menuButton.addEventListener('click', function() {
+      menuButton.classList.toggle('header__menu-button--closed');
+      menuButton.classList.toggle('header__menu-button--opened');
 
-//       // Проверяем, открыто ли меню
-//       if (menuButton.classList.contains('header__menu-button--opened')) {
-//         navigation.classList.add('main-navigation__list--opened');
-//         navigation.classList.remove('main-navigation__list--closed');
-//       } else {
-//         navigation.classList.add('main-navigation__list--closed');
-//         navigation.classList.remove('main-navigation__list--opened');
-//       }
-//     });
-//   });
-// });
+      // Проверяем, открыто ли меню
+      if (menuButton.classList.contains('header__menu-button--opened')) {
+        navigation.classList.add('main-navigation__list--opened');
+        navigation.classList.remove('main-navigation__list--closed');
+      } else {
+        navigation.classList.add('main-navigation__list--closed');
+        navigation.classList.remove('main-navigation__list--opened');
+      }
+    });
+  });
+});
