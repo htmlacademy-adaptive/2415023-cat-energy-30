3a1be3672bc2b5db08b6cdbc3ef58e476dbb07d3
Anastasia <an.gorskova.bg@gmail.com>
2024-01-13 17:37:31 +0100

    добавляет сетки формы для планшетов и десктопа (без проверки на PP)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,21 +1,19 @@
 /* в этот файл добавляет скрипты */
-document.addEventListener('DOMContentLoaded', function() {
-  const menuButtons = document.querySelectorAll('.header__menu-button');
+document.addEventListener('DOMContentLoaded', () => {
+  const menuButton = document.querySelector('.header__menu-button');
   const navigation = document.querySelector('.main-navigation__list');
 
-  menuButtons.forEach(function(menuButton) {
-    menuButton.addEventListener('click', function() {
-      menuButton.classList.toggle('header__menu-button--closed');
-      menuButton.classList.toggle('header__menu-button--opened');
+  menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('header__menu-button--closed');
+    menuButton.classList.toggle('header__menu-button--opened');
 
-      // Проверяем, открыто ли меню
-      if (menuButton.classList.contains('header__menu-button--opened')) {
-        navigation.classList.add('main-navigation__list--opened');
-        navigation.classList.remove('main-navigation__list--closed');
-      } else {
-        navigation.classList.add('main-navigation__list--closed');
-        navigation.classList.remove('main-navigation__list--opened');
-      }
-    });
+    // Проверяем, открыто ли меню
+    if (menuButton.classList.contains('header__menu-button--opened')) {
+      navigation.classList.add('main-navigation__list--opened');
+      navigation.classList.remove('main-navigation__list--closed');
+    } else {
+      navigation.classList.add('main-navigation__list--closed');
+      navigation.classList.remove('main-navigation__list--opened');
+    }
   });
 });
