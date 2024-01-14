140be512d207a1ca21d64203c89a24acd72d5fac
Anastasia <an.gorskova.bg@gmail.com>
2024-01-14 22:33:17 +0100

    проверяет все страницы на переполнение

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -56,26 +56,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   /* проверка на переполнение */
-  // function getTextNodesIn(elem, opt_fnFilter) {
-  //   var textNodes = [];
-  //   if (elem) {
-  //     for (var nodes = elem.childNodes, i = nodes.length; i--;) {
-  //       var node = nodes[i],
-  //         nodeType = node.nodeType;
-  //       if (nodeType == 3) {
-  //         if (!opt_fnFilter || opt_fnFilter(node, elem)) {
-  //           if (node.data.trim() !== '') {
-  //             textNodes.push(node.data.trim() !== '' ? node : '');
+  function getTextNodesIn(elem, opt_fnFilter) {
+    var textNodes = [];
+    if (elem) {
+      for (var nodes = elem.childNodes, i = nodes.length; i--;) {
+        var node = nodes[i],
+          nodeType = node.nodeType;
+        if (nodeType == 3) {
+          if (!opt_fnFilter || opt_fnFilter(node, elem)) {
+            if (node.data.trim() !== '') {
+              textNodes.push(node.data.trim() !== '' ? node : '');
 
-  //           }
-  //         }
-  //       } else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
-  //         textNodes = textNodes.concat(getTextNodesIn(node, opt_fnFilter));
-  //       }
-  //     }
-  //   }
-  //   return textNodes;
-  // }
+            }
+          }
+        } else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
+          textNodes = textNodes.concat(getTextNodesIn(node, opt_fnFilter));
+        }
+      }
+    }
+    return textNodes;
+  }
 
-  // getTextNodesIn(document.body).forEach((item) => { item.textContent = "У меня есть несколько клиентов, У меня есть несколько клиентов, У меня есть несколько клиентов, У меня есть несколько клиентов, которые записались ко мне на стрижку, не успев даже зайти в салон." })
+  // getTextNodesIn(document.body).forEach((item) => { item.textContent = "У меня есть несколько клиентов" })
+
+  getTextNodesIn(document.body).forEach((item) => { item.textContent = "У меня есть несколько клиентов, которые записались ко мне на стрижку, не успев даже зайти в салон. У меня есть несколько клиентов, У меня есть несколько клиентов, У меня есть несколько клиентов, У меня есть несколько клиентов, которые записались ко мне на стрижку, не успев даже зайти в салон." })
 });
